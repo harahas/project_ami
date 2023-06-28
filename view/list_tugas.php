765613a9c5eb666138ebdd662050db32fc8136fe
harahas <hamni.rahma00@gmail.com>
2023-06-28 15:53:33 +0700

    tambah

--- view/list_tugas.php
+++ view/list_tugas.php
@@ -1,17 +1,16 @@
 <?php 
 include '../model/tugas_harian.php';
-$data = select("SELECT * FROM tugas_harian where matkul = 'kalkulus'");
-
+$data   = select("SELECT * FROM tugas_harian");
+$data2  = select("SELECT * FROM tugas_minggun");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <table border="1" cellspacing="0" cellpadding="2">
+<!-- header -->
+<?php include '../layout/header.php'; ?>
+<!-- header -->
+
+    <h1>LIST TUGAS</h1>
+    <a href="tambah_tugas.php" class="btn btn-primary">Tambah Tugas </a>
+    <!-- Table Tugas Harian -->
+    <table class="table table-striped">
         <thead>
             <tr>
                 <td>No</td>
@@ -32,5 +31,32 @@ $data = select("SELECT * FROM tugas_harian where matkul = 'kalkulus'");
             <?php endforeach; ?>
         </tbody>
     </table>
-</body>
-</html>
+    <br></br>
+
+    <!-- Table Mingguan -->
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <td>No</td>
+                <td>Mata Kuliah</td>
+                <td>Tugas</td>
+                <td>Deadline</td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $no = 1; ?>
+            <?php foreach($data2 as $row): ?>
+            <tr>
+                <td><?= $no++; ?></td>
+                <td><?= $row["matkul"]; ?></td>
+                <td><?= $row["tugas"]; ?></td>
+                <td><?= date("l, d-M-Y", strtotime($row["deadline"])); ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+    <br></br>
+    <!-- footer -->
+    <?php include '../layout/footer.php' ?>
+     <!-- footer -->
+    
